9a9049deb986f692d3af312be3992fed0986debe
Gakunzikimana <gakunzigabriella@gmail.com>
2023-02-08 20:33:01 +0100

    created list of shows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import showsData from  './shows-data'
 
+function Show (props) {
+  return (
+    <div className='show'>
+      <img className='show-image'
+      src={props.showObject.image.medium} alt={ props.showObject.className}/>
+    
+      <h1>props.showObject.name</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <h1>The Best TV Shows App</h1>
+      <show showObject={showsData[0]}></show>
+
+      
+      
     </div>
   );
 }
