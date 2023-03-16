b22f60674c7c67edfc5f893a9554151e49004dc3
Gakunzikimana <gakunzigabriella@gmail.com>
2023-03-16 16:44:58 +0100

    removing UL bullets(my works)

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,17 +3,14 @@ function Header() {
     return(
         <div className="Header">
         <div className="Logo"></div>
-        <ul className="Navigation">
-       <li>
-        <Link to="/">Home</Link>
-        </li>
-        <li>
-        <Link to="/Favorites">Favorites</Link>
-        </li>
-        <li>
-        <Link to="/Upcoming">Upcoming</Link>
-        </li>
+        <div className="Navigation">
+        <ul class="nobull">
+       <li><Link to="/">Home</Link></li>
+        <li><Link to="/Favorites">Favorites</Link></li>
+        <li><Link to="/Upcoming">Upcoming</Link></li>
         </ul>
+        </div>
+
       <div className="Search"></div> 
         </div>
         )
