ee45c4da46aa3b1a6f32d320700d23a71d808b8f
Gakunzikimana <gakunzigabriella@gmail.com>
2023-03-28 20:15:06 +0200

    others on showdata

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -8,6 +8,11 @@ return (
         <h1>{ show.name }</h1>
         <img src={show.image.medium}/>
         <h2>language: {show.language}</h2>
+        <h3>genre: {show.genres}</h3>
+        <h4>status: {show.status}</h4>
+        <h5>runtime: {show.runtime}
+        averageruntime: {show.averageRuntime}</h5>
+        <h6>rating: {show.average}</h6>
     </div>
 )
 }
